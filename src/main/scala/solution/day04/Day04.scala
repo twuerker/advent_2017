2a573931ae66464db8e89f16e2bef46b330bdd3a
Taylor Wuerker <taylor_wuerker@comcast.com>
2017-12-05 14:37:48 -0500

    Day 4

--- src/main/scala/solution/day04/Day04.scala
+++ src/main/scala/solution/day04/Day04.scala
@@ -4,9 +4,70 @@ import adventofcode._
 
 object Day04 extends Day(4) {
 
-  def problemA(input: String): Int = ???
+  def hasNoDuplicateWords(passphrase: String): Boolean = {
+    val words = passphrase.split(" ")
+    def checkNextWord(remaining: Seq[String], seen: Set[String]): Boolean =
+      remaining.headOption match {
+        case Some(word) =>
+          if (seen.contains(word)) {
+            false
+          } else {
+            checkNextWord(remaining.tail, seen + word)
+          }
+        case None => true
+      }
+    checkNextWord(words, Set.empty[String])
+  }
+
+  def hasNoAnagrams(passphrase: String): Boolean = {
+    val words = passphrase.split(" ")
+    def checkNextWord(remaining: Seq[String], seen: Set[String]): Boolean =
+      remaining.headOption match {
+        case Some(word) =>
+          val sortedWord = word.sorted
+          if (seen.contains(sortedWord)) {
+            false
+          } else {
+            checkNextWord(remaining.tail, seen + sortedWord)
+          }
+        case None => true
+      }
+    checkNextWord(words, Set.empty[String])
+
+  }
+
+  /**
+    * Problem A
+    * @param input
+    * @return
+    */
+  def problemA(input: String): Int = {
+    input.split(lineSeparator).map { passphrase =>
+      hasNoDuplicateWords(passphrase) match {
+        case true => 1
+        case false => 0
+      }
+    }.sum
+  }
 
   val solutionA = problemA(input)
   finalAnswer(solutionA.toString, A)
+
+  /**
+    * Problem B
+    * @param input
+    * @return
+    */
+  def problemB(input: String): Int = {
+    input.split(lineSeparator).map { passphrase =>
+      hasNoAnagrams(passphrase) match {
+        case true => 1
+        case false => 0
+      }
+    }.sum
+  }
+
+  val solutionB = problemB(input)
+  finalAnswer(solutionB.toString, B)
 
 }
